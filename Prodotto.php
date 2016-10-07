d52a849094cdc90d40158a891bb7309caf64edf7
Francesco Rullo <White900@users.noreply.github.com>
2016-10-07 23:30:23 +0200

    Prodotto
    Definisce un oggetto di tipo prodotto possiede le property nomeProdotto, urlPaginaProdotto, urlImgProdotto e elenco delle caratteristiche del prodotto.

---
+++ Prodotto.php
@@ -0,0 +1,37 @@
+<?php 
+
+class Prodotto {
+	
+	private $nomeProdotto = "";
+	private $urlPaginaProdotto = "";
+	private $urlImgProdotto = "";
+	//TODO inserire variabili per contenere le caratteristiche del prodotto
+	
+	//Metodo costruttore di Produttore
+	public function __construct($nomeProdotto, $urlPaginaProdotto, $urlImgProdotto) { 
+		
+		$this->$nomeProdotto = $nomeProdotto;
+		$this->$urlPaginaProdotto = $urlPaginaProdotto;
+		$this->$urlImgProdotto = $urlImgProdotto;
+	}
+	
+	public function __set($variable, $value) {
+	
+        //echo 'Setto ' . $variable . ' a ' . $value;
+        $this->$variable = $value;
+    }
+
+    public function __get($variable) {
+    
+        if(isset($this->$variable)){
+            return $this->data[$variable];
+            
+        }
+        
+        else{
+            die('Variabile sconosciuta');
+        }
+    }
+}
+
+?>
